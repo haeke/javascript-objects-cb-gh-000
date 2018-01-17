f93bdcc623a98273a3d459fc384978ab2459b225
edwin rivera <waehake@gmail.com>
2018-01-17 03:32:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {
   NERD: 'ESP',
   Kendrick: 'DNA',
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+    playlist.assign({}, artistName, songTitle);
+    return playlist;
+}
