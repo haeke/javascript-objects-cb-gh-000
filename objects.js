7f3e238f2efe6204b19db398db5a3e94b86c649f
edwin rivera <waehake@gmail.com>
2018-01-17 03:36:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    return Object.assign({}, artistName, songTitle);
+    return playlist.assign({}, artistName, songTitle);
 }
